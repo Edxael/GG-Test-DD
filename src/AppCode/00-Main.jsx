be55410cff86e695980a7d8739da7b70d125ab1a
Edxael <zerphyr.r@gmail.com>
2018-05-22 21:51:21 -0600

    update tests

--- src/AppCode/00-Main.jsx
+++ src/AppCode/00-Main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export default class extends React.Component{
+    state = { total: 0 }
     render(){
+
+        const addOne = () => { 
+            this.setState({ total: this.state.total + 1 }) 
+        }
+
         return(
-            <div>
-                <h1>Main C.</h1>
-                <p>Leo</p>
+            <div className="main-cont" >
+                <h1>Add One</h1>
+                <h3>Value: { this.state.total }</h3>
+                <button className="btn-add-1" onClick={ addOne } >Add One</button>
             </div>
         )
     }
 }
+
--- src/MyApp.test.js
+++ src/MyApp.test.js
@@ -3,17 +3,32 @@ import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
  
- 
- 
+
  
 // ===[ Testing <TextComponet/> ]===============================
 import Main from './AppCode/00-Main'
  
-describe('Run test:', () => { 
+describe('Runing test for <Main /> Componet:', () => { 
     Enzyme.configure({ adapter: new Adapter() })
+    const Wrapper = Enzyme.shallow(<Main />)
     
     it('Renders the <Main /> component', () => {
-      const Wrapper = Enzyme.shallow(<Main />)
       expect.anything(Wrapper)
     })
+
+    it('Test if initial value on state.total = 0', () => {
+      expect(Wrapper.state().total).toEqual(0)
+    })
+
+    it('finds the Main <div> container', () => {
+      Wrapper.find('.main-cont')
+    })
+
+    it('Test if when button click changes state to 1', () => {
+      Wrapper.find('.btn-add-1').simulate('click')
+      expect(Wrapper.state().total).toEqual(1)
+    })
+
 })
+
+
